aa28d6206e60ca01aac4770fa66e39d26e0ce4d4
Ivan Chalov <mortigenus@gmail.com>
2019-05-07 22:28:43 +0300

    Add Day 12

--- AoC/main.swift
+++ AoC/main.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+public func readInput() throws -> String {
+    let input: String
+    if CommandLine.arguments.count == 4 && CommandLine.arguments[2] == "cli" {
+        let currentDirectoryURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+        var url = URL(fileURLWithPath: CommandLine.arguments[3], relativeTo: currentDirectoryURL)
+        url.appendPathComponent("Resources/input.txt")
+
+        input = try String(contentsOf: url)
+    } else {
+        guard let path = Bundle.main.path(forResource: "input", ofType: "txt") else {
+            fatalError("Put input for the task into \"input.txt\" file")
+        }
+
+        input = try String(contentsOfFile:path)
+    }
+
+    return input.trimmingCharacters(in: .whitespacesAndNewlines)
+}
+
