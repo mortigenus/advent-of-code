224ae938b39d72d4ae12eac455ee721e5963eae8
Ivan Chalov <mortigenus@gmail.com>
2020-12-04 13:13:07 +0100

    Refactor Day 4

--- Sources/advent-of-code/Solutions/2020/Solution_2020_04.swift
+++ Sources/advent-of-code/Solutions/2020/Solution_2020_04.swift
@@ -7,61 +7,50 @@
 
 import Foundation
 
-typealias Passport = [String:String]
+private typealias Passport = [String:String]
+extension Passport {
+    init(string: String) {
+        self = Dictionary(
+            uniqueKeysWithValues: string
+                .split(whereSeparator: \.isWhitespace)
+                .map { $0.split(separator: ":").map(String.init) }
+                .map { (key: $0[0], value: $0[1]) })
+    }
+}
+
+extension Passport {
+    var requiredKeys: Set<String> {
+        ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    }
+
+    var hasRequiredFields: Bool {
+        Set(self.keys).isSuperset(of: requiredKeys)
+    }
+
+    var hasValidValues: Bool {
+        self["byr"]?.matches("^(19[2-9]\\d|200[0-2])$") ?? false
+            && self["iyr"]?.matches("^(201\\d|2020)$") ?? false
+            && self["eyr"]?.matches("^(202\\d|2030)$") ?? false
+            && self["hgt"]?.matches("^((1[5-8]\\d|19[0-3])cm|(59|6\\d|7[0-6])in)$") ?? false
+            && self["hcl"]?.matches("^#[0-9a-f]{6}$") ?? false
+            && self["ecl"]?.matches("^(amb|blu|brn|gry|grn|hzl|oth)$") ?? false
+            && self["pid"]?.matches("^\\d{9}$") ?? false
+    }
+}
 
 struct Solution_2020_04: Solution {
     var input: Input
     func run() throws {
         let input = try self.input.get()
-            .split(omittingEmptySubsequences: false, whereSeparator: \.isNewline)
-            .map(String.init)
-            .reduce(into: [Passport()]) { acc, x in
-                if x == "" {
-                    acc.append(Passport())
-                    return
-                }
-                let fields = x.split(whereSeparator: \.isWhitespace).map { $0.split(separator: ":").map(String.init) }
-                fields.forEach { keyValueArray in
-                    acc[acc.index(before: acc.endIndex)][keyValueArray[0]] = keyValueArray[1]
-                }
-            }
+            .components(separatedBy: "\n\n")
+            .map(Passport.init(string:))
 
         // ------- Part 1 -------
-        let requiredKeys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-        let part1 = input
-            .filter { passport in
-                requiredKeys.reduce(true) { acc, x in acc && passport[x] != nil }
-            }
-            .count
+        let part1 = input.filter(\.hasRequiredFields).count
         print(part1)
 
         // ------- Part 2 -------
-        let part2 = input
-            .filter { passport in
-                guard let byr = passport["byr"], byr.matches("^(19[2-9]\\d|200[0-2])$") else {
-                    return false
-                }
-                guard let iyr = passport["iyr"], iyr.matches("^(201\\d|2020)$") else {
-                    return false
-                }
-                guard let eyr = passport["eyr"], eyr.matches("^(202\\d|2030)$") else {
-                    return false
-                }
-                guard let hgt = passport["hgt"], hgt.matches("^((1[5-8]\\d|19[0-3])cm|(59|6\\d|7[0-6])in)$") else {
-                    return false
-                }
-                guard let hcl = passport["hcl"], hcl.matches("^#[0-9a-f]{6}$") else {
-                    return false
-                }
-                guard let ecl = passport["ecl"], ecl.matches("^(amb|blu|brn|gry|grn|hzl|oth)$") else {
-                    return false
-                }
-                guard let pid = passport["pid"], pid.matches("^\\d{9}$") else {
-                    return false
-                }
-                return true
-            }
-            .count
+        let part2 = input.filter(\.hasValidValues).count
         print(part2)
 
         // ------- Test -------
--- Sources/advent-of-code/Solutions/Common/String+Regex.swift
+++ Sources/advent-of-code/Solutions/Common/String+Regex.swift
@@ -12,16 +12,3 @@ extension String {
         return self.range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil
     }
 }
-
-//guard let byr = passport["byr"], byr.matches("^(19[2-9]\\d|200[0-2])$") else {
-//    return false
-//}
-//guard let iyr = passport["iyr"], iyr.matches("^(201\\d|2020)$") else {
-//    return false
-//}
-//guard let eyr = passport["eyr"], eyr.matches("^(202\\d|2030)$") else {
-//    return false
-//}
-//guard let hgt = passport["hgt"], hgt.matches("^((1[5-8]\\d|19[0-3])cm|(59|6\\d|7[0-6])in)$") else {
-//    return false
-//}
