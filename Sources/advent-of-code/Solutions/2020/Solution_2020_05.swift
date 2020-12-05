309f57096f16bfbd306b04c6b0bb9a77b20af16d
Ivan Chalov <mortigenus@gmail.com>
2020-12-05 14:26:23 +0100

    Refactor Day 5

--- Sources/advent-of-code/Solutions/2020/Solution_2020_05.swift
+++ Sources/advent-of-code/Solutions/2020/Solution_2020_05.swift
@@ -7,25 +7,6 @@
 
 import Foundation
 
-private func pow(_ a: Int, _ b: Int) -> Int {
-    Int(pow(Double(a), Double(b)))
-}
-
-private func parseNumber(from string: String, one: Character, zero: Character) -> Int {
-    string
-        .reduce((sum: 0, power: string.count - 1)) { acc, char in
-            switch char {
-            case one:
-                return (acc.sum + pow(2, acc.power), acc.power - 1)
-            case zero:
-                return (acc.sum, acc.power - 1)
-            default:
-                fatalError("Incorrect Input")
-            }
-        }
-        .sum
-}
-
 struct Solution_2020_05: Solution {
     var input: Input
     func run() throws {
@@ -34,12 +15,22 @@ struct Solution_2020_05: Solution {
             .map(String.init)
 
         // ------- Part 1 -------
-        let passes: [Int] = input
-            .map { pass in
-                let row = parseNumber(from: String(pass.dropLast(3)), one: "B", zero: "F")
-                let column = parseNumber(from: String(pass.suffix(3)), one: "R", zero: "L")
-                return row * 8 + column
-            }
+        func parseNumber(from string: String) -> Int {
+            string.reversed()
+                .reduce((sum: 0, power: 1)) { acc, char in
+                    switch char {
+                    case "B", "R":
+                        return (acc.sum + acc.power, acc.power * 2)
+                    case "F", "L":
+                        return (acc.sum, acc.power * 2)
+                    default:
+                        fatalError("Incorrect Input")
+                    }
+                }
+                .sum
+        }
+        let passes = input
+            .map(parseNumber(from:))
             .sorted()
         let part1 = passes.last!
         print(part1)
